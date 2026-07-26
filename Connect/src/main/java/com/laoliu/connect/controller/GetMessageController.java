94514e61a64476ac82bdd597d42dbe630486599f
Aliens983 <255169814@qq.com>
2026-07-26 21:40:35 +0800

    feat:Use random port to achieve load balance.

--- Connect/src/main/java/com/laoliu/connect/controller/GetMessageController.java
+++ Connect/src/main/java/com/laoliu/connect/controller/GetMessageController.java
@@ -1,7 +1,10 @@
 package com.laoliu.connect.controller;
 
+import cn.hutool.core.util.RandomUtil;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -23,14 +27,27 @@ public class GetMessageController {
 
     private final RestTemplate restTemplate;
 
-    public GetMessageController(RestTemplate restTemplate) {
+    private final DiscoveryClient discoveryClient;
+
+    public GetMessageController(RestTemplate restTemplate, DiscoveryClient discoveryClient) {
         this.restTemplate = restTemplate;
+        this.discoveryClient = discoveryClient;
     }
 
     @PostMapping
     public String getMessageFromConnected(@RequestParam String id){
+        //1.获取实例列表
+        List<ServiceInstance> instances = discoveryClient.getInstances("connected");
+        if (instances.isEmpty()){
+            return "Error";
+        }
+        //2.从实例列表中获取一个实例
+        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+        URI uri = instance.getUri();
+
+
         ResponseEntity<String> exchange = restTemplate.exchange(
-                "http://localhost:8081/getMessage?secret={id}",
+                uri+"/getMessage?secret={id}",
                 HttpMethod.GET,
                 null,
                 String.class,
--- Connected/src/main/java/com/laoliu/connect/connected/controller/MessageController.java
+++ Connected/src/main/java/com/laoliu/connect/connected/controller/MessageController.java
@@ -1,5 +1,7 @@
 package com.laoliu.connect.connected.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author 25516
  */
+@Slf4j
 @RestController
 @RequestMapping
 public class MessageController {
+
+    @Value("${server.port}")
+    private int port;
+
     @GetMapping("/getMessage")
     public String getMessage(@RequestParam String secret){
         if("111".equals(secret)){
+            log.info("Someone is trying to get message.the port is {}",port);
             return "Fuck all.";
         } else if ("222".equals(secret)) {
             return "What the hell are you doing?";
